e2549254765e5fb5c9bd4af8b60cefd135356b73
YEBBLEE <leeyeb9458@gmail.com>
2023-06-03 16:14:55 +0900

    ♻️ 로우레벨 리팩토링

--- ch6-testable/6-1.js
+++ ch6-testable/6-1.js
@@ -8,29 +8,35 @@
  * (즉, 인자로 전달받도록 )
  */
 export function printOwing(invoice, console, clock) {
-  let outstanding = 0;
+  printBanner(console);
+  const outstanding = calculateOutstanding(invoice);
+  recordDuedate(invoice, clock);
+  printDetails(invoice, outstanding, console);
+}
 
+function printDetails(invoice, outstanding, console) {
+  console.log(`name: ${invoice.customer}`);
+  console.log(`amount: ${outstanding}`);
+  console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+}
+
+function printBanner(console) {
   console.log("***********************");
   console.log("**** Customer Owes ****");
   console.log("***********************");
+}
 
-  // calculate outstanding
-  for (const o of invoice.orders) {
-    outstanding += o.amount;
-  }
+function calculateOutstanding(invoice) {
+  return invoice.orders.reduce((sum, order) => (sum += order.amount), 0);
+}
 
-  // record due date
-  const today = clock.today;
+function recordDuedate(invoice) {
+  const today = new Date();
   invoice.dueDate = new Date(
     today.getFullYear(),
     today.getMonth(),
     today.getDate() + 30
   );
-
-  //print details
-  console.log(`name: ${invoice.customer}`);
-  console.log(`amount: ${outstanding}`);
-  console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
 }
 
 const invoice = {
